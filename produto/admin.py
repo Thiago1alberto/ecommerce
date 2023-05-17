05e195c2e1af64755187eb5e416c2c96d805bd81
Thiago Alberto <71457360+Thiago1alberto@users.noreply.github.com>
2023-05-17 18:03:03 -0300

    models

--- produto/admin.py
+++ produto/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from produto.models import Produto
 
 # Register your models here.
+admin.site.register(Produto)
--- produto/models.py
+++ produto/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
 # Create your models here.
+"""             
+        Variacao:
+            nome - char
+            produto - FK Produto
+            preco - Float
+            preco_promocional - Float
+            estoque - Int
+"""
+
+
+class Produto(models.Model):
+    nome = models.CharField(max_length=255)
+    descricao_curta = models.TextField(max_length=300)
+    descricao_Longa = models.TextField()
+    imagem = models.ImageField(
+        upload_to='produto_imagens/%y/%m/', blank=True, null=True)
+    slug = models.SlugField(unique=True)
+    preco_marketing = models.FloatField(default=0)
+    preco_marketing_promocional = models.FloatField(default=0)
+    tipo = models.CharField(
+        default="v",
+        max_length=1,
+        choices=(
+            ('V', 'Variação'),
+            ('S', 'Simples'),
+        )
+    )
+
+    @staticmethod
+    def resize_image(img, new_width=800):
+        print(img.name)
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        max_image_size = 800
+        if self.imagem:
+            self.resize_image(self.imagem, max_image_size)
+
+    def __str__(self):
+        return self.nome
