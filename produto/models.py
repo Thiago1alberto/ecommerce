fc635abd5a0459ac033f36a486ce9fe555a089fa
Thiago Alberto <71457360+Thiago1alberto@users.noreply.github.com>
2023-05-17 22:59:22 -0300

    Pedido

--- produto/models.py
+++ produto/models.py
@@ -1,14 +1,7 @@
 from django.db import models
-
-# Create your models here.
-"""             
-        Variacao:
-            nome - char
-            produto - FK Produto
-            preco - Float
-            preco_promocional - Float
-            estoque - Int
-"""
+from PIL import Image
+import os
+from django.conf import settings
 
 
 class Produto(models.Model):
@@ -31,13 +24,49 @@ class Produto(models.Model):
 
     @staticmethod
     def resize_image(img, new_width=800):
-        print(img.name)
+        img_full_path = os.path.join(settings.MEDIA_ROOT, img.name)
+        img_pil = Image.open(img_full_path)
+        original_width, original_height = img_pil.size
+
+        if original_width <= new_width:
+            img_pil.close()
+            return
+
+        new_height = round((new_width * original_height) / original_width)
+
+        new_img = img_pil.resize((new_width, new_height), Image.LANCZOS)
+        new_img.save(
+            img_full_path,
+            optimize=True,
+            quality=50
+        )
 
     def save(self, *args, **kwargs):
+        # if not self.slug:
+        #    slug = f'{slugify(self.nome)}'
+        #   self.slug = slug
+
         super().save(*args, **kwargs)
+
         max_image_size = 800
+
         if self.imagem:
             self.resize_image(self.imagem, max_image_size)
 
     def __str__(self):
         return self.nome
+
+
+class Variacao(models.Model):
+    produto = models.ForeignKey(Produto, on_delete=models.CASCADE)
+    nome = models.CharField(max_length=50, blank=True, null=True)
+    preco = models.FloatField()
+    preco_promocional = models.FloatField(default=0)
+    estoque = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return self.nome or self.produto.nome
+
+    class Meta:
+        verbose_name = 'Variação'
+        verbose_name_plural = 'Variações'
